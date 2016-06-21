80838e1b18e7af64a0127045da648266f8217426
SZKL-Developer <SZKL-Developer@SZKL-DEVELOP>
2016-06-21 13:30:38 +0800

    添加项目文件。

---
+++ SuperHGZPrint/ReportDesignerForm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Linq;
+using System.Windows.Forms;
+using DevExpress.XtraBars;
+
+namespace SuperHGZPrint
+{
+    public partial class ReportDesignerForm : DevExpress.XtraBars.Ribbon.RibbonForm
+    {
+        public ReportDesignerForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
